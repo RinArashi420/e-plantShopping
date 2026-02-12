07bc19ec0ddff3346f4adf3e6740c782428a4da7
Kyle Flaharty <kflaharty676@gmail.com>
2026-02-12 14:18:15 -0500

    This finishes up task 2.

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,28 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const {name, image, cost } = action.payload; //destructer product details from the action payload
+        //check if the item is in the cart by comparing names
+        const existingItem = state.items.find(item => item.name === name);
+        if(existingItem) {
+            //if it exists, increase
+            existingItem.quantity++
+        } else {
+            //if it does not exist, add it to cart with quantity 1
+            state.items.push({ name, image, cost, quantity: 1});
+        }
     },
     removeItem: (state, action) => {
+        state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-
-    
+        const { name, quantity } = action.payload; //destructure the data 
+        //find item in cart that matches the name
+        const itemToUpdate = state.items.find(item => item.name === name);
+        //if it exists update the quantity to new quantity.
+        if(itemToUpdate) {
+            itemToUpdate.quantity = quantity;
+        }
     },
   },
 });
